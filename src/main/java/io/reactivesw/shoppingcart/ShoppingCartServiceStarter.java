c2f56a540622d81fe723f28d10e3432fa0848159
JaneLi <jane.li@go6d.com>
2016-09-14 16:48:22 +0800

    gradle update for submodules

--- src/main/java/io/reactivesw/shoppingcart/ShoppingCartServiceStarter.java
+++ src/main/java/io/reactivesw/shoppingcart/ShoppingCartServiceStarter.java
@@ -1,7 +1,5 @@
 package io.reactivesw.shoppingcart;
 
-import io.reactivesw.shoppingcart.ShoppingCartServiceStarter;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
